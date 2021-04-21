e03fac96e7967b7908dd8b96b3ac30172378ef18
Rixafy <rixafy@gmail.com>
2021-04-21 15:53:18 +0200

    Add BentoBox dependency

--- src/main/kotlin/net/minecord/testplugin/TestPlugin.kt
+++ src/main/kotlin/net/minecord/testplugin/TestPlugin.kt
@@ -3,6 +3,7 @@ package net.minecord.testplugin
 import net.minecord.testplugin.listener.TestListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.annotation.dependency.Dependency
 import org.bukkit.plugin.java.annotation.plugin.Description
 import org.bukkit.plugin.java.annotation.plugin.Plugin
 import org.bukkit.plugin.java.annotation.plugin.author.Author
@@ -10,6 +11,7 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author
 @Plugin(name = "TestPlugin", version = "1.0")
 @Description("BentoBox test plugin for events")
 @Author("Rixafy")
+@Dependency("BentoBox")
 class TestPlugin : JavaPlugin() {
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(TestListener(this), this)
